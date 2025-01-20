851088c00af5ac4406bcc52bafd2b05139ad9622
Abdul Azis <ziss.dev@gmail.com>
2025-01-20 09:41:25 +0700

    fix: adjust base alert dialog detail

--- lib/modules/user/presentation/controllers/create_user_controller.dart
+++ lib/modules/user/presentation/controllers/create_user_controller.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_boilerplate/modules/user/data/models/user_model.dart';
+import 'package:flutter_boilerplate/modules/user/data/models/user_payload.dart';
 import 'package:flutter_boilerplate/modules/user/data/repositories/user_repository.dart';
 import 'package:flutter_boilerplate/shared/utils/alert_dialog_helper.dart';
+import 'package:flutter_boilerplate/shared/utils/app_utils.dart';
 import 'package:flutter_boilerplate/shared/utils/result_state/result_state.dart';
+import 'package:flutter_boilerplate/shared/widgets/app_error_bottom_sheet.dart';
 import 'package:get/get.dart';
 
 class CreateUserController extends GetxController {
@@ -29,31 +32,29 @@ class CreateUserController extends GetxController {
   final createState = Rx<ResultState<UserModel>>(const ResultState.initial());
 
   Future<void> addUser() async {
-    AlertDialogHelper.showCreateUserSuccessDialog();
+    if (formKey.currentState?.validate() == false) return;
+    createState.value = const ResultState.loading();
 
-    // if (formKey.currentState?.validate() == false) return;
-    // createState.value = const ResultState.loading();
+    final payload = UserPayload(
+      username: unameController.text.trim(),
+      firstName: fNameController.text.trim(),
+      lastName: lNameController.text.trim(),
+      password: passController.text.trim(),
+    );
 
-    // final payload = UserPayload(
-    //   username: unameController.text.trim(),
-    //   firstName: fNameController.text.trim(),
-    //   lastName: lNameController.text.trim(),
-    //   password: passController.text.trim(),
-    // );
+    final result = await _repository.addUser(payload);
+    result.fold((failure) {
+      createState.value = const ResultState.failed();
 
-    // final result = await _repository.addUser(payload);
-    // result.fold((failure) {
-    //   createState.value = const ResultState.failed();
+      final message = AppUtils.getErrorMessage(failure.error?.errors) ?? '';
+      if (Get.isBottomSheetOpen == false) {
+        AppUtils.showBottomSheet(AppErrorBottomSheet(message: message));
+      }
+    }, (data) {
+      createState.value = ResultState.success(data);
 
-    //   final message = AppUtils.getErrorMessage(failure.error?.errors) ?? '';
-    //   if (Get.isBottomSheetOpen == false) {
-    //     AppUtils.showBottomSheet(AppErrorBottomSheet(message: message));
-    //   }
-    // }, (data) {
-    //   createState.value = ResultState.success(data);
-
-    //   Get.back(result: true);
-    //   AlertDialogHelper.showCreateUserSuccessDialog();
-    // });
+      Get.back(result: true);
+      AlertDialogHelper.showCreateUserSuccessDialog();
+    });
   }
 }
